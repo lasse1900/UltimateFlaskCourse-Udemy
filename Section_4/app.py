edd044261acb669afc30770c9f59bf2dd94a7972
Lauri Kyttälä <lauri.kyttala@etteplan.com>
2023-06-18 15:06:06 +0300

    added Section_4 - basic function on add_food page

---
+++ Section_4/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, g, request
+import sqlite3
+
+app = Flask(__name__)
+
+def connect_db():
+    sql = sqlite3.connect('data.db')
+    sql.row_factory = sqlite3.Row
+    return sql
+
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/view')
+def view():
+    return render_template('day.html')
+
+@app.route('/food', methods=['GET','POST'])
+def food():
+    if request.method == 'POST':
+        # name = request.form['food-name']
+        # protein = request.form['protein']
+        # carbohydrates = request.form['carbohydrates']
+        # fat = request.form['fat']
+        return '<h1>Name: {} Protein: {} Carbs: {} Fat: {}</h1>'.format(request.form['food-name'], \
+            request.form['protein'], request.form['carbohydrates'], request.form['fat']                                                     )
+    return render_template('add_food.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
